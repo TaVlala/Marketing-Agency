b842ca94fb774dd387dbe0f2b77553dc8ecf3842
TaVlala <dtavlalashvili@hotmail.com>
2026-05-09 22:26:24 +0400

    fix: ignore ESLint warnings during Vercel build

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import { withPayload } from '@payloadcms/next/withPayload';
 
 const nextConfig: NextConfig = {
   reactStrictMode: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   images: {
     loader: 'custom',
     loaderFile: './src/lib/cloudinary-loader.ts',
